6aade4b27daae12e6420a5f9b529aaaa0357167f
alexisbarros <apenasalexis@gmail.com>
2021-01-01 22:48:21 -0300

    development: create home component

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+// Modules
+import { NativeRouter, Route } from "react-router-native";
+
+// Components
+import Home from './components/home/Home'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+
+    <NativeRouter>
+
+      <Route exact path="/" component={Home} />
+
+    </NativeRouter>
+
+  );
+};
---
+++ components/home/Home.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+// Modules
+import { View } from 'react-native';
+import { Text, Button } from 'react-native-elements';
+import { Link } from 'react-router-native'
+
+const home = () => {
+
+    return(
+
+        <View 
+            style={{
+                justifyContent: 'center',
+                flex: 1,
+            }}
+        >
+
+            <View
+                style={{
+                    padding: 20
+                }}
+            >
+                <Text h1>Bingo app</Text>
+
+                <Text
+                    style={{
+                        fontSize: 18,
+                        color: 'gray',
+                        marginTop: 16
+                    }}
+                >O app para jogar bingo com toda sua família e amigos.</Text>
+
+                <Link 
+                    to='/how-to'
+                    underlayColor="#f0f4f7"
+                    style={{
+                        marginTop: 16
+                    }}
+                >
+                    <Text
+                        style={{
+                            fontSize: 16,
+                        }}
+                    >Como jogar?</Text>
+                </Link>
+            </View>
+            
+            <View
+                style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    width: '100%',
+                    padding: 20
+                }}
+            >
+
+                <Button
+                    title='Cartela'
+                    style={{
+                        width: '100%'
+                    }}
+                />
+
+                <Button
+                    title='Roleta'
+                    style={{
+                        width: '100%',
+                        marginTop: 10,
+                    }}
+                    buttonStyle={{
+                        backgroundColor: '#606060'
+                    }}
+                />
+
+            </View>
+        </View>
+
+    )
+
+};
+
+export default home
